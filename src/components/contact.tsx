034b72e9816f8782e637597d51ae47f1c1e68425
Chris <ahnkhit@gmail.com>
2025-05-05 16:22:00 +0200

    contact responsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,39 +3,39 @@ import Link from "next/link"
 
 export default function Contact() {
     return (
-        <div id="contact" className="flex flex-col items-center justify-center h-auto bg-black text-white py-16">
-            <h1 className="text-5xl font-bold mb-12">Contact</h1>
+        <div id="contact" className="flex flex-col items-center justify-center bg-black text-white">
+            <h1 className="text-[2.5vw] font-bold mb-[5vw]">Contact</h1>
 
             <div className="flex justify-center gap-[10%] w-full max-w-4xl px-8">
                 <Link href="tel:+33766254068" className="flex flex-col items-center group">
-                    <div className="w-[130px] h-[130px] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
-                        <Image width={100} height={100} src="/images/iconPhone.png" alt="Téléphone" className="w-12 h-12" />
+                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                        <Image width={100} height={100} src="/images/iconPhone.png" alt="Téléphone" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-3 text-base opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         07 66 25 40 68
                     </p>
                 </Link>
                 <Link href="https://www.instagram.com/bak_khit" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center group">
-                    <div className="w-[130px] h-[130px] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
-                        <Image width={100} height={100} src="/images/iconInstagram.png" alt="Instagram" className="w-12 h-12" />
+                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                        <Image width={100} height={100} src="/images/iconInstagram.png" alt="Instagram" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-3 text-base opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         bak_khit
                     </p>
                 </Link>
                 <Link href="https://www.linkedin.com/in/anh-hiep-nguyen-54a446331/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center group">
-                    <div className="w-[130px] h-[130px] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
-                        <Image width={100} height={100} src="/images/iconLinkedin.webp" alt="LinkedIn" className="w-12 h-12" />
+                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                        <Image width={100} height={100} src="/images/iconLinkedin.webp" alt="LinkedIn" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-3 text-base opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         Anh Hiep Nguyen
                     </p>
                 </Link>
                 <Link href="mailto:ahnkhit@gmail.com" className="flex flex-col items-center group">
-                    <div className="w-[130px] h-[130px] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
-                        <Image width={100} height={100} src="/images/iconMail.webp" alt="Email" className="w-12 h-12" />
+                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                        <Image width={100} height={100} src="/images/iconMail.webp" alt="Email" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-3 text-base opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         ahnkhit@gmail.com
                     </p>
                 </Link>
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Projects() {
     return (
         <div id="projects" className="flex flex-col items-center justify-center h-[110vh] w-full bg-black justify-self-center">
-            <h1 className="text-[5vw] font-bold mb-12 text-white">Projects</h1>
+            <h1 className="text-[2.5vw] font-bold mb-12 text-white">Projects</h1>
             <div className="flex flex-wrap justify-center gap-8 rounded-lg w-[80vw]">
                 <Link href="https://super-simon-v1.vercel.app" target="_blank" className="w-[20vw] h-[15vw] bg-black flex justify-center items-center rounded-lg text-white text-xl group relative overflow-hidden border-2 border-white">
                     <Image width={100} height={100} src="/images/SuperSimon.png" alt="Super Simon" className="w-[90%]" />
