64b135d87047c9801e6a865a9bee70a78b8967a9
Chris <ahnkhit@gmail.com>
2025-05-05 13:53:18 +0200

    AboutMe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "../components/header"
-import AboutMe from "../components/aboutMe"
 import Projects from "../components/projects"
 import Skills from "../components/skills"
 import Contact from "../components/contact"
