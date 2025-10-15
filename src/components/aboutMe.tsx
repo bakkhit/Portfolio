bd9fe4bac416f1fafe762a84ef026abd0d76453e
Anh Hiep Nguyen <anh.hiep@example.com>
2025-10-15 11:19:00 +0200

    responsive...

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 export default function AboutMe() {
     return (
-        <div  id="aboutMe" className="aboutMe flex items-center justify-center text-center h-[110vh]">
+        <div  id="aboutMe" className="aboutMe flex items-center justify-center text-center h-[100vh]">
             <div className="relative w-150 overflow-hidden">
                 <Image layout="responsive" width={100} height={100} src="/images/khit.png" alt="khit" className="khitAM" />
-                <div className="khitShadow absolute bottom-0 left-0 w-full h-100 bg-gradient-to-t from-black to-transparent"></div>
+                <div className="khitShadow absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent"></div>
             </div>
 
-            <div className="textAM flex flex-col items-center justify-center w-[50vw] h-fit">
-                <p className="salut text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 text-[2vw]">Salut !</p>
-                <p className="textAm text-white max-w-2xl text-[1vw]">
+            <div className="textAM flex flex-col items-center justify-center w-[50vw] h-fit translate-y-[-50%]">
+                <p className="salut text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 text-[2vw]">Salut !</p><br></br>
+                <p className="textAm text-white max-w-2xl text-[1vw] w-[40vw]">
                     {`Moi, c'est`} <span className="textAm2 text-transparent bg-clip-text bg-gradient-to-r from-green-600 via-green-500 to-purple-500 text-[1vw]">Anh Hiep NGUYEN</span>, étudiant en première année de DNMADE Numérique. Passionné par le développement web, je jongle entre le code, la danse et les sessions de sport, que ce soit en salle ou en pleine nature. Fan de jeux vidéo et de musique, j’aime créer autant que découvrir de nouvelles expériences !
                 </p>
             </div>
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -8,34 +8,34 @@ export default function Contact() {
 
             <div className="flex justify-center gap-[10%] w-full max-w-4xl px-8">
                 <Link href="tel:+33766254068" className="flex flex-col items-center group">
-                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                    <div className="phoneImg w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
                         <Image width={100} height={100} src="/images/iconPhone.png" alt="Téléphone" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="phoneNumber mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         07 66 25 40 68
                     </p>
                 </Link>
                 <Link href="https://www.instagram.com/bak_khit" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center group">
-                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                    <div className="instagramImg w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
                         <Image width={100} height={100} src="/images/iconInstagram.png" alt="Instagram" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="instagramText mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         bak_khit
                     </p>
                 </Link>
                 <Link href="https://www.linkedin.com/in/anh-hiep-nguyen-54a446331/" target="_blank" rel="noopener noreferrer" className="flex flex-col items-center group">
-                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                    <div className="linkedinImg w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
                         <Image width={100} height={100} src="/images/iconLinkedin.webp" alt="LinkedIn" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="linkedinText mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         Anh Hiep Nguyen
                     </p>
                 </Link>
                 <Link href="mailto:ahnkhit@gmail.com" className="flex flex-col items-center group">
-                    <div className="w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
+                    <div className="mailImg w-[10vw] h-[10vw] bg-white flex justify-center items-center rounded-full transition-transform duration-300 hover:scale-105">
                         <Image width={100} height={100} src="/images/iconMail.webp" alt="Email" className="object-cover w-[5vw]" />
                     </div>
-                    <p className="mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
+                    <p className="mailText mt-[1vw] text-[1vw] opacity-80 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap text-center">
                         ahnkhit@gmail.com
                     </p>
                 </Link>
